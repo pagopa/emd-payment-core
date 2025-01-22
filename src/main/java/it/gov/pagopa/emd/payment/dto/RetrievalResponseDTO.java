e35b475a2b66c02341767e0d01cbb55d52ea657a
BaldiVi <vittorio.baldi@emea.nttdata.com>
2025-01-22 17:18:52 +0100

    Add annotation Jackson for null element

--- src/main/java/it/gov/pagopa/emd/payment/dto/RetrievalResponseDTO.java
+++ src/main/java/it/gov/pagopa/emd/payment/dto/RetrievalResponseDTO.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.emd.payment.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import it.gov.pagopa.emd.payment.model.base.RetrievalBase;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -10,5 +11,6 @@ import lombok.experimental.SuperBuilder;
 @Data
 @SuperBuilder
 @NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class RetrievalResponseDTO extends RetrievalBase {
 }
