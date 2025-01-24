0a96fe41ff778a55c57c35d8f770713d7fe52c32
BaldiVi <vittorio.baldi@emea.nttdata.com>
2025-01-24 17:13:47 +0100

    remove unused import

--- src/test/java/it/gov/pagopa/emd/payment/faker/RetrievalFaker.java
+++ src/test/java/it/gov/pagopa/emd/payment/faker/RetrievalFaker.java
@@ -1,7 +1,6 @@
 package it.gov.pagopa.emd.payment.faker;
 
 
-import it.gov.pagopa.emd.payment.dto.RetrievalResponseDTO;
 import it.gov.pagopa.emd.payment.model.Retrieval;
 
 public class RetrievalFaker {
