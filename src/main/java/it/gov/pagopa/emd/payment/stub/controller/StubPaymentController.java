1908ef64021a0c57e80a514441dbc175b8889d35
stedelia <144045955+stedelia@users.noreply.github.com>
2025-03-12 12:28:27 +0100

    fix: promote to UAT (#10)
    Co-authored-by: DanieleRanaldo <124155243+DanieleRanaldo@users.noreply.github.com>
    Co-authored-by: BaldiVi <vittorio.baldi@emea.nttdata.com>

---
+++ src/main/java/it/gov/pagopa/emd/payment/stub/controller/StubPaymentController.java
@@ -0,0 +1,27 @@
+package it.gov.pagopa.emd.payment.stub.controller;
+
+
+import it.gov.pagopa.emd.payment.dto.RetrievalRequestDTO;
+import it.gov.pagopa.emd.payment.dto.RetrievalResponseDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Mono;
+
+
+@RequestMapping("/stub/emd/payment")
+public interface StubPaymentController {
+
+    @PostMapping("/retrievalTokens/{entityId}")
+    Mono<ResponseEntity<RetrievalResponseDTO>> retrievalTokens(@Valid @PathVariable String entityId, @Valid @RequestBody RetrievalRequestDTO retrievalRequestDTO);
+
+    @GetMapping("/retrievalTokens/{retrievalId}")
+    Mono<ResponseEntity<RetrievalResponseDTO>> getRetrieval(@Valid @PathVariable String retrievalId) ;
+
+    @GetMapping("/token")
+    Mono<ResponseEntity<Void>> generateDeepLink(@Valid @RequestParam String retrievalId, @Valid @RequestParam String fiscalCode, @Valid @RequestParam String noticeNumber);
+
+    @GetMapping("/payment")
+    Mono<ResponseEntity<Void>> generateDeepLink();
+
+}
